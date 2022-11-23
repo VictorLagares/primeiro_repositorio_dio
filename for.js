f1c82cb770599fb2f097a787693ac8a5ab23b28a
Victor Lagares <103151987+VictorLagares@users.noreply.github.com>
2022-11-22 22:27:17 -0300

    Delete for.js

--- for.js
+++
@@ -1,7 +0,0 @@
-/*Crie um programa que imprima 11 vezes a frase " Hello World!" utilizando uma estrutura de repetição while.*/
-num = 0
-for (let i = 0; i <= 100; i++) {
-    if(i % 2 === 0){
-    console.log([i])
-    }
-}
